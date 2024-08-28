2b14981802173ad24f92631c5909d29a56bdc5a4
Yash Khati <160757711+Yash-codeAndDev@users.noreply.github.com>
2024-08-28 07:00:33 +0000

    Minimum Swaps to sort array

---
+++ GeeksForGeeks/MinimumSwapsToSort/Solution.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Collections;
+
+class Pair{
+    int first;
+    int second;
+
+    public Pair(int first, int second){
+        this.first = first;
+        this.second = second;
+    }
+
+    public String toString() {
+        return "(" + first + ", " + second + ")";
+    }
+}
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        int[] arr = {6 , 4, 2, 14, 8, 10, 12, 16};
+        // System.out.println("Given Array -> "+Arrays.toString(arr));
+
+        ArrayList<Pair> originalList = new ArrayList<>();
+        for(int i=0; i < arr.length; i++){
+            originalList.add(new Pair(arr[i] , i));
+        }
+        System.out.println("Elements and Index List -> "+originalList);
+
+        Collections.sort(originalList , new Comparator<Pair>(){
+            public int compare(Pair p1, Pair p2) {
+                return Integer.compare(p1.first, p2.first);
+            }
+        } );
+
+        System.out.println("Sorted  List -> "+originalList);
+
+        boolean[] vis = new boolean[arr.length];
+        int minSwaps = 0;
+
+        for(int i=0; i<arr.length; i++ )
+        {
+
+            if(vis[i] == true || originalList.get(i).second == i){
+                continue;
+            }
+            else{
+                int j = i;
+                int cycleLen = 0;
+                while(vis[j] != true){
+
+                    vis[j] = true;
+                    j = originalList.get(j).second;
+                    cycleLen += 1;
+                }
+
+                minSwaps += cycleLen-1;
+            }
+        }
+
+        System.out.println("Minimum Swaps : "+minSwaps);
+    }
+}
---
+++ GeeksForGeeks/readme.md
@@ -0,0 +1,30 @@
+<h2><a href="https://www.geeksforgeeks.org/problems/minimum-swaps/1">Minimum Swaps to Sort</a></h2><h3>Medium</h3><hr><div><p>Given an array of n distinct elements. Find the <em>minimum number of swaps required to sort the array</em> in strictly increasing order.
+</p>
+
+<p>&nbsp;</p>
+<p><strong class="example">Example 1:</strong></p>
+
+<pre style="position: relative;"><strong>Input:</strong> nums = [2,8,5,4]
+<strong>Output:</strong> 1
+<strong>Explanation:</strong> Swap 8 with 4
+<div class="open_grepper_editor" title="Edit &amp; Save To Grepper"></div></pre>
+
+<p><strong class="example">Example 2:</strong></p>
+
+<pre style="position: relative;"><strong>Input:</strong> nums =[10, 19, 6, 3, 5]
+<strong>Output:</strong> 4
+<div class="open_grepper_editor" title="Edit &amp; Save To Grepper"></div></pre>
+
+<p><strong class="example">Example 3:</strong></p>
+
+<pre style="position: relative;"><strong>Input:</strong> nums = [10, 19, 6, 3, 5]
+<strong>Output:</strong> 2
+<div class="open_grepper_editor" title="Edit &amp; Save To Grepper"></div></pre>
+
+<p>&nbsp;</p>
+<p><strong>Constraints:</strong></p>
+
+<ul>
+	<li><code>1 &lt;= n &lt;= 10<sup>5</sup></code></li>
+	<li><code>1 &lt;= nums[i] &lt;= 10<sup>6</sup></code></li>
+</ul>
