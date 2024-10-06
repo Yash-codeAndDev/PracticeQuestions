f375a4a14f9b5aa6d34c2483ebf6cbf38feed2a3
Yash Khati <160757711+Yash-codeAndDev@users.noreply.github.com>
2024-10-06 05:04:20 +0000

    Minumum in Roatated Sorted Array

---
+++ BinarySearch/FindMinimumInRotatedSortedArray/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int arr[] = {8, 9, 10, 1, 2, 3, 4, 5 ,6 , 7};
+        
+        System.out.println(findMinimum(arr));
+    }
+
+    public static int findMinimum(int arr[])
+    {
+
+        int n = arr.length;
+
+        int low = 0;
+        int high = n-1;
+        int min = Integer.MAX_VALUE;
+        while(low <= high){
+
+            int mid = low + (high-low)/2;
+
+            if(arr[low] <= arr[mid]) // if left is sorted
+            {
+                if(arr[low] <= min){
+                    min = arr[low];
+                }
+                low = mid + 1;
+            }
+            else if(arr[mid]<= arr[high]){
+                
+                if(arr[mid] <= min){
+                    min = arr[mid];
+                }
+                high = mid-1;
+            }
+        }
+
+        return min;
+    }
+}
